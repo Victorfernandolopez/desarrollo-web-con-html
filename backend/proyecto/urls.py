881fed91bec6ad04c888ee5ac7f383f7ef63c1f9
Victorfernandolopez <victorfernandolopez1988@gmail.com>
2025-02-19 23:44:35 -0300

    inclucion de las urls de la aplicacion API

--- backend/proyecto/urls.py
+++ backend/proyecto/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for proyecto project.
+from django.contrib import admin  # Importa el módulo de administración de Django
+from django.urls import path, include  # Importa las funciones path e include del módulo django.urls
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
+# Define las rutas URL principales para el proyecto
 urlpatterns = [
+    # Asocia la URL 'admin/' con la interfaz de administración de Django
     path('admin/', admin.site.urls),
+    
+    # Asocia la URL 'api/' con las rutas definidas en el archivo api/urls.py
+    path('api/', include('api.urls')),
 ]
